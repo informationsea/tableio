3dfca30b6926c111ece0e7c79c2adfcf54cd13ea
Y.Okamura <okamura@informationsea.info>
2015-06-02 23:48:44 +0900

    version 0.0.3

--- publish-local.sh
+++ publish-local.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-./gradlew clean
-./gradlew build generatePomFileForMavenJavaPublication pP publishToMavenLocal
+
+./gradlew clean || exit 1
+./gradlew build || exit 1
+./gradlew generatePomFileForMavenJavaPublication || exit 1
+./gradlew preparePublication publishToMavenLocal || exit 1
--- publish.sh
+++ publish.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-./gradlew clean build generatePomFileForMavenJavaPublication pP publish
+
+./gradlew clean || exit 1
+./gradlew build || exit 1
+./gradlew generatePomFileForMavenJavaPublication || exit 1
+./gradlew preparePublication publish || exit 1
+
